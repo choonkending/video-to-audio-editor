84d1158fb45a91104a476aa98c36b31f589cc5ce
Ken Ding <aenkil@gmail.com>
2021-02-05 23:51:34 +1100

    Use config in Main
    - Add required environment variables in docker-compose file

--- src/main/scala/editor/Main.scala
+++ src/main/scala/editor/Main.scala
@@ -1,8 +1,19 @@
 package editor
 
+import editor.config.{Config, Environment}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Scala App running ...")
+    println("\n🙏 🙏 🙏 Welcome to the Audio Visual Team 🙏 🙏 🙏\n")
+
+    val env = Environment(sys.env)
+
+    Config.getProductionConfig(env) match
+      case Left(e: Throwable) =>
+        System.err.println(s"Failed to start editor: ${e.getMessage}")
+        throw e
+      case Right(config) =>
+        println("\nWonderful, we have our required environment variables available 🎉\n")
   }
 }
 
