bb4f12ea0a8000fd569a803f04e7551b425b95af
Ken Ding <aenkil@gmail.com>
2021-02-18 22:36:45 +1100

    Convert Main to use IOApp
    https://typelevel.org/cats-effect/datatypes/ioapp.html

--- src/main/scala/editor/Main.scala
+++ src/main/scala/editor/Main.scala
@@ -1,23 +1,24 @@
 package editor
 
+import cats.effect._
 import editor.config.{Config, Environment}
 import editor.audio._
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = {
     println("\n🙏 🙏 🙏 Welcome to the Audio Visual Team 🙏 🙏 🙏\n")
 
     val env = Environment(sys.env)
 
     Config.getProductionConfig(env) match
       case Left(e) =>
-        System.err.println(s"Failed to start editor: ${e.errorMessage}")
+        IO(System.err.println(s"Failed to start editor: ${e.errorMessage}")).as(ExitCode.Error)
       case Right(config) =>
         println("\nWonderful, we have our required environment variables available 🎉\n")
         val converterService = new ConverterService(config, FFMPEG.run)
-        converterService.convert().unsafeRunSync() match {
-          case FFMPEGExecutionSuccess => println("Successfully ran the converter service")
-          case FFMPEGExecutionFailure => println("Failed to run the converter service")
+        converterService.convert().flatMap {
+          case FFMPEGExecutionSuccess => IO(println("Successfully ran the converter service")).as(ExitCode.Success)
+          case FFMPEGExecutionFailure => IO(println("Failed to run the converter service")).as(ExitCode.Error)
         }
   }
 }
